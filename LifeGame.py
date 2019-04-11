55e064736f49c223532fe8cc4542fa65a441b6eb
sogwms <sogwyms@gmail.com>
2019-04-11 16:09:43 +0800

    [add] display frame by frame

--- LifeGame.py
+++ LifeGame.py
@@ -3,14 +3,16 @@
 
 import lifeRule
 import lifeMap
-
+import time
 
 if __name__ == '__main__':
-    map = lifeMap.Map()
+    map = lifeMap.Map(66, 66)
     map.random(0.1)
     for i in range(100):
         lifeRule.game_cycle(map)
-    map.showloop(10)
+        time.sleep(0.01)
+        map.show(10)
+    #map.showloop(10)
     
     
         
